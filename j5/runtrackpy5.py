3598352b7db8570f6169163dbf45920fdc737e79
sofiane-benali <sofiane.benali13@gmail.com>
2023-02-13 11:26:40 +0100

    ajout de job 5

--- j5/runtrackpy5.py
+++ j5/runtrackpy5.py
@@ -44,10 +44,16 @@ def c_chiffrement(message,clef):
 
 print(c_chiffrement("hello wolrd",10))
 
-#jo 4
+#job 5
 
-
-
+def calculmarche(nb, h) :  
+    print(f"Pour {nb} marches de {h}cm, il parcourt {nb*h*2*5*7/100.0:.2f}m par semaine !") 
+   
+nombre_marches = int(input("Combien de marches ?"))  
+hauteur_marche = int(input("Hauteur d'une marche (cm) ?"))  
+  
+calculmarche(nombre_marches, hauteur_marche)
+    
 
 
 
