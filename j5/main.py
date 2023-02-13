111c5da3ef1e3a423a600472caab3c94c442235f
sofiane <sofiane@Sofiane-MacBook-Pro.local>
2023-02-13 09:06:45 +0100

    ajout de jobs

--- j5/main.py
+++ j5/main.py
@@ -3,5 +3,54 @@ def hellotoi():
     askname = input("Quel est votre nom ?")
     return print("Votre nom est ",askname)
 
-hellotoi()
 #job 2
+def rectangledessin(h,l):
+
+    print("|", "-"*(l-2), "|")
+    for i in range(h-2):
+        print("|", " "*(l-2), "|")
+    print("|", "-"*(l-2), "|")
+
+
+
+#job 3
+def tapis(n):
+    for i in range(n + 1):
+        for j in range(n + 1):
+            if i == j:
+                print(" ", end=" ")
+            else:
+                print("#", end=" ")
+        print()
+
+
+#job 4
+def chiffrement(lettre,clef):
+    key = ord(lettre) + clef
+    if key > 122:
+        key -= 26
+    return chr(key)
+
+print(chiffrement("o",3))
+
+def c_chiffrement(message,clef):
+    chiffr = ""
+    for lettre in message:
+        if lettre == " ":
+            chiffr += " "
+        else: chiffr += chiffrement(lettre,clef)
+
+    return chiffr
+
+print(c_chiffrement("hello wolrd",10))
+
+#jo 4
+
+
+
+
+
+
+
+
+
